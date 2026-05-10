26709799fa91f5f57d8f2a5c05e83e06ce75c21a
LolaSimone <losimone@frba.utn.edu.ar>
2026-05-10 18:12:23 -0300

    arreglo de controllers

--- src/main/java/ar/edu/utn/dds/k3003/controllers/NecesidadController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controllers/NecesidadController.java
@@ -7,8 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController
+
 @RequestMapping("/necesidades")
+@RestController
 public class NecesidadController {
 
     @Autowired
