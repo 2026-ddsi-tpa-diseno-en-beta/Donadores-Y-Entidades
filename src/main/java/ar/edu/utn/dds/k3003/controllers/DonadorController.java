12f94c7aa1fe9b4c86eeee2c80c7e3e232609e53
LolaSimone <losimone@frba.utn.edu.ar>
2026-05-12 08:42:29 -0300

    arreglo de controllers

--- src/main/java/ar/edu/utn/dds/k3003/controllers/DonadorController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controllers/DonadorController.java
@@ -2,12 +2,14 @@ package ar.edu.utn.dds.k3003.controllers;
 
 import ar.edu.utn.dds.k3003.Fachada;
 import ar.edu.utn.dds.k3003.catedra.dtos.donadoresYEntidades.DonadorDTO;
+import ar.edu.utn.dds.k3003.catedra.dtos.donadoresYEntidades.EstadoDonadorEnum;
 import ar.edu.utn.dds.k3003.catedra.dtos.donadoresYEntidades.QuejaDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/donadores")
@@ -44,5 +46,33 @@ public class DonadorController {
         );
         
         return ResponseEntity.ok(fachada.agregarQueja(nuevaQueja));
+    }
+
+    @GetMapping("/{id}/quejas")
+    public ResponseEntity<List<QuejaDTO>> listarQuejas(@PathVariable String id) {
+        return ResponseEntity.ok(fachada.obtenerQuejasDe(id));
+    }
+
+    @PatchMapping("/{id}/estado")
+    public ResponseEntity<DonadorDTO> actualizarEstado(@PathVariable String id, @RequestBody Map<String, String> body) {
+        String estadoString = body.get("estado");
+        EstadoDonadorEnum nuevoEstado = EstadoDonadorEnum.valueOf(estadoString);
+        return ResponseEntity.ok(fachada.modificarEstado(id, nuevoEstado));
+    }
+
+    @PatchMapping("/{id}/categoria")
+    public ResponseEntity<DonadorDTO> actualizarCategoria(@PathVariable String id, @RequestBody Map<String, String> body) {
+        String nuevaCat = body.get("categoria");
+        return ResponseEntity.ok(fachada.modifcarCategoria(id, nuevaCat));
+    }
+
+    @GetMapping("/{id}/puede-donar")
+    public ResponseEntity<Boolean> puedeDonar(@PathVariable String id) {
+        return ResponseEntity.ok(fachada.puedeDonar(id));
+    }
+
+    @GetMapping("/{id}/estadisticas")
+    public ResponseEntity<Object> obtenerStats(@PathVariable String id) {
+        return ResponseEntity.ok(fachada.estadisticasDonador(id));
     }
 }
