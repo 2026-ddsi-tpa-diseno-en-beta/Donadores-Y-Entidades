e84b16f80be4c438361b4787bd33978b93e6f62b
LolaSimone <losimone@frba.utn.edu.ar>
2026-06-07 18:47:12 -0300

    controllers y +

---
+++ src/main/java/ar/edu/utn/dds/k3003/controllers/SistemaController.java
@@ -0,0 +1,44 @@
+package ar.edu.utn.dds.k3003.controllers;
+
+import ar.edu.utn.dds.k3003.Fachada;
+import ar.edu.utn.dds.k3003.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController 
+public class SistemaController {
+
+    @Autowired
+    private Fachada fachada;
+
+    @Autowired
+    private DonadoresRepository donadorRepository;
+    @Autowired
+    private EntidadesRepository entidadesRepository;
+    @Autowired
+    private QuejaRepository quejaRepository;
+    @Autowired
+    private NecesidadMaterialRepository necesidadRepository;
+
+    @DeleteMapping("/base-de-datos")
+    public ResponseEntity<String> limpiarBaseDeDatos() {
+        
+        quejaRepository.deleteAll();
+        necesidadRepository.deleteAll();
+        donadorRepository.deleteAll();
+        entidadesRepository.deleteAll();
+        
+        return ResponseEntity.ok("¡Base de datos limpia y reluciente! ✨");
+    }
+
+    @GetMapping("/donadores")
+    public ResponseEntity<?> obtenerTodosLosDonadores() {
+        return ResponseEntity.ok(fachada.listarDonadores());
+    }
+
+    @GetMapping("/entidades")
+    public ResponseEntity<?> obtenerTodasLasEntidades() {
+        return ResponseEntity.ok(fachada.listarEntidades());
+    }
+}
