bd7493cdd18067f54e51276b7c6f5a78e36bccde
LolaSimone <losimone@frba.utn.edu.ar>
2026-06-12 13:51:27 -0300

    .

--- src/main/java/ar/edu/utn/dds/k3003/exceptions/ApiExceptionHandler.java
+++ src/main/java/ar/edu/utn/dds/k3003/exceptions/ApiExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 
-@RestControllerAdvice
+//@RestControllerAdvice
 public class ApiExceptionHandler {
 
     private final DonadorMetricas metrics;
